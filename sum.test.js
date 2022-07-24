ff2a14089a9aa6f0f90da49f3ae042900ee4a5a5
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-24 18:16:50 +0530

    Jest Test Case

--- sum.test.js
+++ sum.test.js
@@ -7,5 +7,21 @@ describe('Test Suite', ()=>{
         expect(sum(2,3)).toBe(5)
     })
 
+    it('To Equal test',()=>{
+        const obj = {}
+        expect(obj).toEqual({});
+    })
+
 })
 
+
+describe('Numbers test',()=>{
+    it('number test',()=>{
+        const val = 2+3;
+        expect(val).toBe(5)
+        expect(val).toBeGreaterThan(4)
+        expect(val).toBeGreaterThanOrEqual(5)
+        expect(val).toBeLessThan(9)
+        expect(val).toBeLessThanOrEqual(5)
+    })
+})
