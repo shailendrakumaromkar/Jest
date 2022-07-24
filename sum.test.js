fe6079d19ee440a163c6986831fbe581c0c75b98
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-24 17:58:45 +0530

    Jest Test Case

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,11 @@
 const sum = require('./sum')
 
-it('sum of 2 number',()=>{
+describe('Test Suite', ()=>{
 
-    expect(sum(2,3)).toBe(5)
+    it('sum of 2 number',()=>{
+
+        expect(sum(2,3)).toBe(5)
+    })
+
 })
+
