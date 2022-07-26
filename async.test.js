8f9d5ff677f784ee7f30f2b533068cc07df32108
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-27 02:03:54 +0530

    updated test case

--- async.test.js
+++ async.test.js
@@ -5,7 +5,7 @@ describe('async test', ()=>{
     it('async test cases', async()=>{
 
         const res = await fetchData(1);
-        expect(res.id).toBe(1)
+        expect(res.id).toBe(1);
 
     })
 })
