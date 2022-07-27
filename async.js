86cfb220de0a24240cfac4ad81476adc2a9c6704
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-28 01:30:23 +0530

    added test cases

--- async.js
+++ async.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const fetchData = async (id) =>{
     const result = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    //console.log(result);
+    //console.log(result)
     return result.data
 };
 
